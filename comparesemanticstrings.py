eb42ebe24b5c9a5a809869587e4c17b1ac6d26b7
Swaroop Kundeti <swaroopkuma@gmail.com>
2017-05-23 16:07:25 -0400

    Update comparesemanticstrings.py

--- comparesemanticstrings.py
+++ comparesemanticstrings.py
@@ -18,9 +18,7 @@ _REGEX = re.compile(r'''
 
 
 def _parse(version):
-    """ Remove all version aliases, e.g. pre-release type 'c' is short
-        for 'rc'.
-    """
+   
     if version[1] == 'a':
         version = (str(version[0]), 'alpha', str(version[2]), )	
     elif version[1] == 'b':
@@ -40,20 +38,14 @@ def compareversions(string1, rule, string2):
 	string1 = string1.lower()
 	string2 = string2.lower()
 
-	# Ensure strings are in right format 
-
 	string1_check = _REGEX.match(string1)
 	#print string1_check
 	if not string1_check:
 		 sys.exit('"%s" is not a valid semantic version' % string2)
 
-        #raise exception('version of string "{!s}" is invalid.'.format(string1))	
-
 	string2_check = _REGEX.match(string2)
 	if not string2_check:
 		sys.exit('"%s" is not a valid semantic version' % string2) 
-        #raise InvalidVersionString('version of string "{!s}" is invalid.'.format(string2))
-
 
 	if rule == "==":
 		return string1 == string2
